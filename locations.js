2d446b5fb9b9d65b09cb3b91ce046b1471022548
Alex Millet <alexander.millet1@marist.edu>
2014-11-18 15:06:41 -0500

    this should work.

---
+++ locations.js
@@ -0,0 +1,34 @@
+
+
+    function Location(name, description, items){
+        this.name = name;
+        this.description = description;
+        this.items = items;
+    
+    };
+    
+    var firstFloor = new Location('First Floor', 'This floor has two doors.... they appear to be locked', 'keycard');
+    var utilitiesRoom = new Location('Utilities Room', 'There seems to be a working keycard encoder', 'Encoded keycard');
+    var elevator = new Location('Elevator', 'Building seems to have 4 floors....', );
+    var locations = [];
+    locations.push(firstFloor, utilitiesRoom, elevator);
+    
+    
+    var connections = [
+      [0,1,1],
+      [1,0,0],
+      [1,0,0],
+    ];
+    
+    var map = {locations: locations, connectionsMatrix: connections};
+    
+    function testThis(){
+      for ( i in locations){
+      if(connections[0][i] === 1){
+        console.log(locations[i].name);
+        }
+      }
+    };
+    
+    testThis();
+    
