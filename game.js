6e33e0cc672eff4abc97251f6dbc837fe606b6bb
Alex Millet <Alexander.Millet1@marist.edu>
2014-12-02 15:19:56 -0500

    Added key game functions and opening screen

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 var player = {
 items: [],
+name: '',
+gender: '',
 
 };
 
@@ -7,8 +9,39 @@ player.pickup = function(item){
   player.items.push(item);
 };
 
-var interpret = function(string){
-var adjS = string.trim();
+	var interpret = function (input)  {
+		console.log("This is working please delete me in your code");
+		// var cmd = {}, tokens = input.trim().toLowerCase().split(" ");
+        // cmd.action = tokens.shift();
+        // cmd.target = tokens.join(" ");
+	    // return cmd;
+
+	};
+
+var gameStep = function(input){
+	var command; // take the user input and put it through the interpreter
+	var result; //run your interpreted command function through execute functionm
+	//need some way of displaying the results on the screen report function
+	var descrip = document.querySelector('#descrip');
+	descrip.innerHTML = "";
+
+	//while (descrip.firstChild) {descrip.remove} goes through a parent and we can remove all children
+	var removeDescrip = document.querySelectorAll()
+	descrip.removeChild(removeDescrip);
+}
 
 
-};
+
+
+var gameStart = function() {
+	var nameInput = document.querySelector('#name');
+	var genderInput = document.querySelector('#gender');
+	nameInput.addEventListener("keyup", function(event){
+		if (event.keyCode === 13) {
+			player.name = nameInput.value;
+			player.gender = genderInput.value;
+			gameStep(this.value);
+		}
+	});
+}
+window.onload = gameStart;
