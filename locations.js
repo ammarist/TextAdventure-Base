767787c4075a08368aa7f6cc14367709a7def252
Alex Millet <Alexander.Millet1@marist.edu>
2014-12-02 15:29:34 -0500

    Added functions to game.js and debugged locations

--- locations.js
+++ locations.js
@@ -4,24 +4,24 @@
         this.name = name;
         this.description = description;
         this.items = items;
-    
+
     };
-    
+
     var firstFloor = new Location('First Floor', 'This floor has two doors.... they appear to be locked', 'keycard');
     var utilitiesRoom = new Location('Utilities Room', 'There seems to be a working keycard encoder', 'Encoded keycard');
-    var elevator = new Location('Elevator', 'Building seems to have 4 floors....', );
+    var elevator = new Location('Elevator', 'Building seems to have 4 floors....' );
     var locations = [];
     locations.push(firstFloor, utilitiesRoom, elevator);
-    
-    
+
+
     var connections = [
       [0,1,1],
       [1,0,0],
       [1,0,0],
     ];
-    
+
     var map = {locations: locations, connectionsMatrix: connections};
-    
+
     function testThis(){
       for ( i in locations){
       if(connections[0][i] === 1){
@@ -29,6 +29,5 @@
         }
       }
     };
-    
+
     testThis();
-    
