f7fa44cd0c1a86e10814ad85df7181c4348c81d4
Alex Millet <Alexander.millet1@marist.edu>
2014-12-06 15:29:06 -0500

    Organized code and added the function call to gameIntro

--- game.js
+++ game.js
@@ -1,60 +1,51 @@
 		
-		var player = {
-						items: [],
-						name: '',
-						gender: '',
-					}
+var player = {
+		items: [],
+		name: '',
+		gender: '',
+}
 
-		player.pickup = function(item){
-  				player.items.push(item);
-					}
+player.pickup = function(item){
+  		player.items.push(item);
+}
 
-	function clearContent(node) {
+function clearContent(node) {
     while (node.hasChildNodes()) {
         node.removeChild(node.firstChild);
     }
 }
 
-
-
-	function itemList(items) {
-				var i, item, inventory;
-    			inventory = document.querySelector("#inventory > ul");
-    			clearContent(inventory);
-    				for (i in player.items) {
-        					item = document.createElement ("li");
-        					item.textContent = player.items[i];
-        					inventory.appendChild(item);
-    						}		
-						}
-
-	function interpret(input) {
-    			var cmd = {}, tokens = input.trim().toLowerCase().split(" ");
-    			cmd.action = tokens.shift();
-    			cmd.target = tokens.join(" ");
-    			return cmd;
-			}
-
-
-	var gameStep = function(input){
-		var command; // take the user input and put it through the interpreter
-		var result; //run your interpreted command function through execute functionm
-		//need some way of displaying the results on the screen report function
-		var descrip = document.querySelector('#descrip');
-		clearContent(descrip);
-		
-
-		//while (descrip.firstChild) {descrip.remove} goes through a parent and we can remove all children
-	
+function itemList(items) {
+		var i, item, inventory;
+    	inventory = document.querySelector("#inventory > ul");
+    	clearContent(inventory);
+    		for (i in player.items) {
+        		item = document.createElement ("li");
+        		item.textContent = player.items[i];
+        		inventory.appendChild(item);
+    		}		
 }
 
+function interpret(input) {
+    	var cmd = {}, tokens = input.trim().toLowerCase().split(" ");
+    	cmd.action = tokens.shift();
+    	cmd.target = tokens.join(" ");
+    	return cmd;
+}
 
+var gameStep = function(input){
+	var command; // take the user input and put it through the interpreter
+	var result; //run your interpreted command function through execute functionm
+		//need some way of displaying the results on the screen report function
+	var descrip = document.querySelector('#descrip');
+	clearContent(descrip);	
+}
 
-	function item (name, numUses, power){
-			this.name = name;
-			this.uses = numUses;
-			this.power = power;
-		}
+function item (name, numUses, power){
+		this.name = name;
+		this.uses = numUses;
+		this.power = power;
+}
 
 var actionBox = document.querySelector("#action")
 
@@ -66,9 +57,9 @@ var gameIntro = function() {
 			event.target.removeEventListener('keyup', listener)
 			player.name = nameInput.value;
 			player.gender = genderInput.value;
-			gameStep;
-		
+			gameStep();	
 		}
 	}
 }
-window.onload = gameIntro;
+
+window.onload = gameIntro();
