a076114aeab9780b21398f2381c08a848be7583b
Alex Millet <Alexander.millet1@marist.edu>
2014-12-06 14:32:06 -0500

    added interpret function, item list function, and still can't get the intro to go away on press of enter

--- game.js
+++ game.js
@@ -1,48 +1,74 @@
-var player = {
-items: [],
-name: '',
-gender: '',
+		
+		var player = {
+						items: [],
+						name: '',
+						gender: '',
+					}
 
-};
+		player.pickup = function(item){
+  				player.items.push(item);
+					}
 
-player.pickup = function(item){
-  player.items.push(item);
-};
+	function clearContent(node) {
+    while (node.hasChildNodes()) {
+        node.removeChild(node.firstChild);
+    }
+}
 
-	var interpret = function (input)  {
-		console.log("This is working please delete me in your code");
-		// var cmd = {}, tokens = input.trim().toLowerCase().split(" ");
-        // cmd.action = tokens.shift();
-        // cmd.target = tokens.join(" ");
-	    // return cmd;
 
-	};
 
-var gameStep = function(input){
-	var command; // take the user input and put it through the interpreter
-	var result; //run your interpreted command function through execute functionm
-	//need some way of displaying the results on the screen report function
-	var descrip = document.querySelector('#descrip');
-	descrip.innerHTML = "";
+	function itemList(items) {
+				var i, item, inventory;
+    			inventory = document.querySelector("#inventory > ul");
+    			clearContent(inventory);
+    				for (i in player.items) {
+        					item = document.createElement ("li");
+        					item.textContent = player.items[i];
+        					inventory.appendChild(item);
+    						}		
+						}
 
-	//while (descrip.firstChild) {descrip.remove} goes through a parent and we can remove all children
+	function interpret(input) {
+    			var cmd = {}, tokens = input.trim().toLowerCase().split(" ");
+    			cmd.action = tokens.shift();
+    			cmd.target = tokens.join(" ");
+    			return cmd;
+			}
+
+
+	var gameStep = function(input){
+		var command; // take the user input and put it through the interpreter
+		var result; //run your interpreted command function through execute functionm
+		//need some way of displaying the results on the screen report function
+		var descrip = document.querySelector('#descrip');
+		clearContent(descrip);
+		
+
+		//while (descrip.firstChild) {descrip.remove} goes through a parent and we can remove all children
 	
 }
 
-var actionBox = document.querySelector('#action')
 
 
+	function item (name, numUses, power){
+			this.name = name;
+			this.uses = numUses;
+			this.power = power;
+		}
 
-var gameStart = function() {
+var actionBox = document.querySelector("#action")
+
+var gameIntro = function() {
 	var nameInput = document.querySelector('#name');
 	var genderInput = document.querySelector('#gender');
-	actionBox.addEventListener("keyup", function(event){
+	var listener = function(event){
 		if (event.keyCode === 13) {
+			event.target.removeEventListener('keyup', listener)
 			player.name = nameInput.value;
 			player.gender = genderInput.value;
-			gameStep(this.value);
+			gameStep;
 		
 		}
-	})
+	}
 }
-window.onload = gameStart;
+window.onload = gameIntro;
