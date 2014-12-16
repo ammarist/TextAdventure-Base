666e3048054fb957a6901db158f6ea4e474132e0
Alex Millet <Alexander.millet1@marist.edu>
2014-12-15 20:43:53 -0500

    go function is Workinggit add game.js added and subtracted alot of functions

--- game.js
+++ game.js
@@ -7,9 +7,20 @@ var player = {
         currentLocationName: locations[0].name,
         currentLocationRow: locations[0].row,
         actions: [],
-        location: locations[0],
+        location: locations[0],//object in an array
 }
 
+player.use = function(item){
+		for(var i = 0; i < player.items.length; i++){
+			if(item == player.items[i]){
+				var x = player.items[i]
+			}
+		if(x.quantity == 1){
+			x = '';
+			alert('Proceed');
+		}
+}
+}
 
 function help(){
 var i, item, inventory;
@@ -24,28 +35,52 @@ var i, item, inventory;
 
 
 function execute(command) {
-	if(command === ''){
-		return false
-	}
-	else{
     player[command.action](command.target);
 }
-}
-player.go =function(input){
-	
 
+player.go =function(input){
+	//for(var i = 0; i < player.items.length; i++){
+		//if(player.items[i] == player.location.requirements){
+
+	for(var i = 0; i < locations.length; i++){
+		if(input == locations[i].name){
+			clearContent();
+			appendDescrip(player.location.description);
+			console.log('THANK GOD!!!');
+			player.currentLocationRow = locations[i].row;
+			player.currentLocationName = locations[i].name;
+			player.location = locations[i];
+		//}
+	//}
+	}
+		//else{
+			
+		//}
+}
+}
+player.goBack = function(row){
+	
 }
 
 player.check = function(input){
         var x = getConnectedLocations(player.currentLocationRow);
-       	//takes the number of the current location row which starts off as 0 runs it through get connected locations and gets available places and stores it
-        
+        appendAvActions('Go ' + x);
 
-    }
+       	//takes the number of the current location row which starts off as 0 runs it through get connected locations and gets available places and stores it
+
+}
+
 
 
 player.pickup = function(item){
-  		player.items.push(item);
+		clearItems();
+		if(player.location.items = item){
+			player.items.push(item);
+		}
+  		else{
+  			console.log('not for you');
+  		}
+  		appendInventory(player.items);
 }
 
 player.shoot = function(){
@@ -58,18 +93,20 @@ player.shoot = function(){
 
 
 function appendDescrip(input){ 
-	var p = document.createElement('p');
-	var node = document.createElement('h1');
-	var textnode = document.createTextNode(input); 
-	node.appendChild(textnode);
-	p.appendChild(node);
-	document.getElementById('scene').appendChild(p);
+	clearContent();
+	var p = document.createElement('p');//creates paragraph 
+	var node = document.createElement('h1');//creates a heading
+	var textnode = document.createTextNode(input);//creates a text node with whatever the program passes through the parameter
+	node.appendChild(textnode);//appends the textnode to the heading
+	p.appendChild(node);//appends the heading to the paragraph
+	document.getElementById('scene').appendChild(p);//appends the paragraph to the scene
 	
 }
 
-function appendAvActions(input){
-	var ul = document.createElement('ul');
-	var textnode = document.createTextNode(input);
+function appendAvActions(){
+	clearActions();
+	var ul = document.createElement('ul');// creates ul
+	var textnode = document.createTextNode(AvActions());// creates the text node containing available actions
 	ul.appendChild(textnode);
 	document.getElementById('avA').appendChild(ul);
 }
@@ -78,16 +115,19 @@ function appendInventory(input){
 	var ul = document.createElement('ul');
 	var textnode = document.createTextNode(input);
 	ul.appendChild(textnode);
-	document.getElementById('inventory').appendChild(ul);
+	document.getElementById('inv').appendChild(ul);
 }
 
 
 function gameStep(action) {
-	clearContent();
-	clearActions();
-    var cmd = interpret(action); 
+	appendAvActions();
+    appendDescrip(player.location.description);
+    var cmd = interpret(action);
     var result = execute(cmd);
     
+    
+    
+   
 }
 function clearActions(){
 	var a = document.getElementById('avA');
@@ -95,7 +135,12 @@ function clearActions(){
 		a.removeChild(a.firstChild);
 	}
 }
-
+function clearItems(){
+	var a = document.getElementById('inv');
+	while (a.firstChild) {
+		a.removeChild(a.firstChild);
+	}
+}
 function clearContent(node) {
     var descrip = document.getElementById('scene');
 	while (descrip.firstChild) {
@@ -120,15 +165,26 @@ function interpret(input) {
     	cmd.target = tokens.join(" ");
     	return cmd;
 }
+function returnGameStepLocation(action) {
+   			 var cmd = interpret(action);
+				var x = cmd.target
+					return x;
+				}		
 
 function gameStart() {
-	clearContent();
-	appendDescrip(player.location.description);
-	appendAvActions(player.location.availableActions);
+	
+	clearActions();
+	appendDescrip(player.location.description);// appends the Home description to the page
+	appendAvActions();//
     var inputBox = document.querySelector("#action");
     inputBox.addEventListener("keyup", function(event) {
         if (event.keyCode === 13) {
-            gameStep(this.value);
+            gameStep(inputBox.value);
+            clearContent();
+            clearActions();
+            appendDescrip(player.location.description);
+            appendAvActions();
+
         }
     });
 }
@@ -144,18 +200,20 @@ function pushToP(x){
     }
 
 var gameIntro = function() {
-	var nameInput = document.querySelector('#name');
-	var genderInput = document.querySelector('#gender');
-		var actionBox = document.querySelector('#action');
-		actionBox.addEventListener('keyup', listener = function(event){
+	var nameInput = document.querySelector('#name');//selects input box by id
+	var genderInput = document.querySelector('#gender');//selects inpu box by id
+		var actionBox = document.querySelector('#action');//selections action box by id
+		actionBox.addEventListener('keyup', listener = function(event){//adds an event listener for 13
 		if (event.keyCode === 13) {
-			x = document.getElementById('action').value
-			player.name = nameInput.value;
-			player.gender = genderInput.value;
-			gameStart();
-
+			x = document.getElementById('action').value//gets the value of action
+			player.name = nameInput.value;//stores the users name on the key up of 13
+			player.gender = genderInput.value;//stores the users gener on the key up of 13
+			clearActions();//clears my actions
+			clearContent();
+			gameStart();//calls gameStart
+			
 		}
-		actionBox.removeEventListener('keyup',listener);
+		actionBox.removeEventListener('keyup', listener);
 	});
 }
 
--- locations.js
+++ locations.js
@@ -1,18 +1,18 @@
 
 
-    function Location(name, description, items, enemies, availableActions, row){
+    function Location(name, description, items, enemies, availableActions, row, requirements){
         this.name = name;
         this.description = description;
         this.items = items;
         this.enemies = enemies;
         this.availableActions = availableActions;
         this.row = row;
+        this.requirements = requirements;
     };
 
-    function AvActions(action1, action2, action3) {
-      var x = [];
-      x.push(action1,action2,action3);
-      return x;
+    function AvActions() {
+      var x = player.location.availableActions;//goes into the location array and selects whatever object the represents the player's location and returns its available actions
+      return x;// returns the content
     }
 
     
@@ -31,19 +31,22 @@
       var silencedPistol = new itemConstructor('Silenced 9mm Pistol', '', 2, 5, 15 );
       var poison = new itemConstructor('Posion', "Slip into a consumable and watch your desired outcome", 5, 5, 1);
       var knife = new itemConstructor('Knife',"Raptor Claw knife. Perfect for a stealth kill", 1, 5, 10);
+      var keycard = new itemConstructor('keycard', 'used for security clearance', 0, 0, 1);
+      var encodedKeycard = new itemConstructor('encodedKeycard', 'can be used for all floors', 0, 0, 1);
+
 
       var gear = [intel, silencedPistol, poison, knife];
 
 
 
-    var lobby = new Location('Lobby', 'This floor has two doors.... they appear to be locked.', 'keycard', 0 , AvActions('pickup ','go'), 1);
-    var utilitiesRoom = new Location('Utilities Room', 'There seems to be a working keycard encoder', 'Encoded keycard', 2, AvActions('pickup','shoot'), 2);
-    var elevator = new Location('Elevator', 'Building seems to have 4 floors....', '' ,0 ,AvActions('go'), 3 );
-    var home = new Location('Home', 'I need to pick up my gear before I go.', gear, 0 , AvActions('pickup gear', 'go lobby'), 0);
-    var firstFloor = new Location('First Floor', "You only have Security clearance for this floor! The receptionist keeps looking up at me, I should be able to get some information out of her.", "Target is in the 2nd office on the 2nd floor ", 0 , AvActions('talk', 'go'), 3);
-    var secondFloor = new Location('Second Floor', "there are two offices which should I go to?", '' , 0 , AvActions('go'), 4 );
-    var officeOne = new Location("First Office", "This is his office... there is medication on his desk", "Pill container", 0 , AvActions("poison", "go"), 5);
-    var officeTwo = new Location('Second Office', "He is here", '', 1, AvActions('shoot', 'knife', 'observe'), 6);    
+    var lobby = new Location('lobby', 'This is the Lobby. There are inate patterns on the wall. This floor has two doors.... the elevator appears to be locked. I see an employees keycard on the ground, but I need a way of encoding it', keycard, 0 , ['pickup keycard ',' go utilities room'], 1, gear);
+    var utilitiesRoom = new Location('utilities room', 'There seems to be a working keycard encoder', encodedKeycard, 2, ['pickup encoded keycard ','shoot ', 'go lobby'], 2, keycard);
+    var elevator = new Location('elevator', 'Building seems to have 4 floors....', '' ,0 ,'go first floor', 3 , encodedKeycard);
+    var home = new Location('home', 'I just got my intel on a new target. The intel should be in my gear. I need to pick up my gear before I go.', gear, 0 ,  'go lobby, pickup gear', 0, gear);
+    var firstFloor = new Location('first floor', "The receptionist keeps looking up at me, I should be able to get some information out of her.", "Target is in the 2nd office on the 2nd floor ", 0 , ['talk ', 'go second floor'], 3, '');
+    var secondFloor = new Location('second floor', "there are two offices which should I go to?", '' , 0 , ['go first office'], 4 , '');
+    var officeOne = new Location("first office", "This is his office... there is medication on his desk", "Pill container", 0 , ["poison pill container ", "go second office"], 5, '');
+    var officeTwo = new Location('second office', "He is here", '', 1, [' shoot', ' knife', ' observe'], 6, '');    
 
     var locations = [];
     locations.push(home, lobby, utilitiesRoom, elevator,  firstFloor, secondFloor, officeOne, officeTwo);
@@ -88,5 +91,17 @@
             }
     }
 
+    function getRowByName(input){
+            var x;
+            for(i = 0; i < locations.length; i++){//loops through locations array
+                    if(input == locations[i].name){// if input is equal to a name in locations
+                         x = i;
+                        return x;//returns the row on connections
+                    }                    
+                    else{
+                      console.log('this ' + input + " does not exist.... try typing again");
+                    }
+            }
+    }
     
 
